f6ee531fe7c1bc38a785a402b9bc46b9cebaed38
Ankit Tewari <ankitt.nic@gmail.com>
2019-07-31 16:24:57 +0200

    Update Week-03.py

--- DataCamp Lectures/Deep Learning in Python/Week-03.py
+++ DataCamp Lectures/Deep Learning in Python/Week-03.py
@@ -1,3 +1,6 @@
+# Architecture, Compilation, Fitting
+
+
 # Import necessary modules
 import keras
 from keras.layers import Dense
@@ -17,3 +20,40 @@ model.add(Dense(32, activation= 'relu'))
 
 # Add the output layer
 model.add(Dense(1))
+
+#compilation
+model.compile(optimizer= 'adam', loss= 'mean_squared_error')
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)
+
+# Fit the model
+model.fit(predictors, target)
+
+
+# Classification tasks
+
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
+
+# Convert the target to categorical: target
+target = to_categorical(df.survived)
+
+# Set up the model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(32, activation= 'relu', input_shape= (n_cols, )))
+
+# Add the output layer
+model.add(Dense(2, activation= 'softmax'))
+
+# Compile the model
+model.compile(optimizer= 'sgd', loss= 'categorical_crossentropy',
+        metrics= ['accuracy'])
+
+# Fit the model
+model.fit(predictors, target)
